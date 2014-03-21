899261ae08e2d486f172a93d2b44d370618ac4a7
kalandin <carlo.lucibello@gmail.com>
2014-03-21 12:36:22 +0100

    some updates

--- include/board.h
+++ include/board.h
@@ -1,6 +1,11 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+#include <vector>
+#include <cassert>
+
+using namespace std;
+
 #include "candy.h"
 
 /**********************
@@ -10,9 +15,19 @@ class Board{
 public:
         int w;  // width
         int h;  // height of the visible part
-        int N;   // height of the whole strip
+        int hS;   // height of the whole strip
+        vector<vector<Candy>> grid;  // the baord matrix
 
         Board() {}
+
+        Board(int width, int height, int heightStrip)
+            : w(width), h(height), hS(heightStrip)  {
+            assert(h <= hS);
+            grid.resize(hS);
+            for(auto& row : grid){
+                row.resize(w);
+            }
+        }
 
         virtual ~Board() {}
 };
